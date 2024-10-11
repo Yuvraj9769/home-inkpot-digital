66a1c2fc525c7466615296092d181598d3f2ccab
Yuvraj <yuvrajs22it@student.mes.ac.in>
2024-10-11 23:47:23 +0530

    navbar update

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-window.onload = function () {
-  Array.from(document.getElementsByClassName("box")).forEach((e) => {
-    let count = 0;
-    const targetValue = parseInt(e.getElementsByTagName("p")[0].textContent);
-
-    const interval = setInterval(() => {
-      count += 1;
-      e.getElementsByTagName("p")[0].textContent = count + " +";
-
-      if (count >= targetValue) {
-        clearInterval(interval);
-      }
-    }, 25);
-  });
-};
-
 let currentIndex = 0;
 const testimonials = document.querySelectorAll(".testimonial");
 
